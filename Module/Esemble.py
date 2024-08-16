3f81ff5fc84533c96f730ec05a8a8cd4decae58e
Kim Juhwan <asas4539@hanyang.ac.kr>
2024-08-16 17:29:43 +0900

    .

---
+++ Module/Esemble.py
@@ -0,0 +1,177 @@
+import joblib
+from sklearn.metrics import *
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+
+class Esemble:
+    def __init__(self, method, X_train, X_val, y_train, y_val, num_rounds, sampling_name):
+        self.method = method
+        self.X_train = X_train
+        self.X_test = X_val
+        self.y_train = y_train
+        self.y_test = y_val
+        self.num_rounds = num_rounds
+        self.name = sampling_name
+
+    def save_dict_to_txt(self, file_path, dictionary):
+        with open(file_path, 'w') as f:
+            for key, value in dictionary.items():
+                f.write(f'{key}: {value}\n')
+
+    def DecisionTree(self, params):
+        bst = DecisionTreeClassifier(**params)
+        bst.fit(self.X_train, self.y_train)
+        y_pred = bst.predict_proba(self.X_test)
+        predictions = y_pred.argmax(axis=1)
+        accuracy = f1_score(self.y_test, predictions, average='binary')
+
+        # joblib.dump(bst, f'File/dt_{self.name}_model.pkl')
+        print("DT F1-Score:", accuracy)
+        return accuracy
+
+    def lightGBM(self, params):
+        bst = LGBMClassifier(**params, n_estimators=self.num_rounds, verbose_eval=100)
+        bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)])
+        y_pred = bst.predict(self.X_test)
+        accuracy = f1_score(self.y_test, y_pred, average='binary')
+
+        # joblib.dump(bst, f'File/lgb_{self.name}_model.pkl')
+        print("lightGBM F1-Score:", accuracy)
+        return accuracy
+
+    def XGBoost(self, params):
+        bst = XGBClassifier(**params, n_estimators=self.num_rounds)
+        bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
+        y_pred = bst.predict(self.X_test)
+        accuracy = f1_score(self.y_test, y_pred, average='binary')
+
+        # joblib.dump(bst, f'File/xgb_{self.name}_model.pkl')
+        print("XGBoost F1-Score:", accuracy)
+        return accuracy
+
+    def CatBoost(self, params):
+        bst = CatBoostClassifier(**params, iterations=self.num_rounds)
+        bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
+        y_pred = bst.predict(self.X_test)
+        accuracy = f1_score(self.y_test, y_pred, average='binary')
+
+        # joblib.dump(bst, f'File/cat_{self.name}_model.pkl')
+        print("CatBoost F1-Score:", accuracy)
+        return accuracy
+
+    def objective(self, trial):
+        if self.method == 0:
+            params = {
+                'criterion': 'entropy',
+                'max_features': trial.suggest_float('max_features', 0.5, 0.99),
+                'max_depth': trial.suggest_int('max_depth', 5, 200),
+            }
+            accuracy = self.DecisionTree(params)
+
+        if self.method == 1:
+            params = {
+                'device': 'cpu',
+                'objective': 'binary',
+                'metric': 'cross_entropy',
+                'boosting_type': 'goss',
+                'early_stopping_rounds': 128,
+
+                'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.1),
+                'max_depth': trial.suggest_int('max_depth', 5, 50),
+                # 'num_leaves': trial.suggest_int('num_leaves', 31, 256),
+            }
+            accuracy = self.lightGBM(params)
+
+        if self.method == 2:
+            params = {
+                'device': 'cuda',
+                'objective': 'binary:logistic',
+                'eval_metric': 'logloss',
+                'booster': 'gbtree',
+                'early_stopping_rounds': 128,
+
+                'eta': trial.suggest_float('eta', 0.01, 0.1),
+                'max_depth': trial.suggest_int('max_depth', 5, 50),
+                # 'max_leaves': trial.suggest_int('max_leaves', 31, 256)
+            }
+            accuracy = self.XGBoost(params)
+
+        if self.method == 3:
+            params = {
+                'task_type': 'CPU',
+                'loss_function': 'Logloss',
+                'eval_metric': 'F1',
+                'grow_policy': 'Depthwise',
+                'early_stopping_rounds': 128,
+
+                'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.1),
+                'depth': trial.suggest_int('depth', 5, 16),
+                # 'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 1, 50),
+            }
+            accuracy = self.CatBoost(params)
+
+        return accuracy
+
+    def save_best_model(self, best_params):
+        if self.method == 0:
+            best_params.update({
+                'criterion': 'entropy',
+            })
+
+            bst = DecisionTreeClassifier(**best_params)
+            bst.fit(self.X_train, self.y_train)
+            joblib.dump(bst, f'File/dt_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/dt_{self.name}_params.txt', best_params)
+            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="binary"):.4f}')
+            print("Model saved!")
+
+        if self.method == 1:
+            best_params.update({
+                'device': 'cpu',
+                'objective': 'binary',
+                'metric': 'cross_entropy',
+                'boosting_type': 'gbrt',
+                'early_stopping_rounds': 128,
+            })
+
+            bst = LGBMClassifier(**best_params)
+            bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)])
+            joblib.dump(bst, f'File/lgb_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/lgb_{self.name}_params.txt', best_params)
+            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="binary"):.4f}')
+            print("Model saved!")
+
+        if self.method == 2:
+            best_params.update({
+                'device': 'cuda',
+                'objective': 'binary:logistic',
+                'eval_metric': 'logloss',
+                'booster': 'gbtree',
+                'early_stopping_rounds': 128,
+            })
+
+            bst = XGBClassifier(**best_params)
+            bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
+            joblib.dump(bst, f'File/xgb_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/xgb_{self.name}_params.txt', best_params)
+            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="binary"):.4f}')
+            print("Model saved!")
+
+        if self.method == 3:
+            best_params.update({
+                'task_type': 'CPU',
+                'loss_function': 'CrossEntropy',
+                'eval_metric': 'F1',
+                'grow_policy': 'Depthwise',
+                'early_stopping_rounds': 128,
+            })
+
+            bst = CatBoostClassifier(**best_params)
+            bst.fit(self.X_train, self.y_train, eval_set=[(self.X_test, self.y_test)], verbose=100)
+            joblib.dump(bst, f'File/cat_{self.name}_model.pkl')
+            self.save_dict_to_txt(f'File/cat_{self.name}_params.txt', best_params)
+            print(f'{f1_score(self.y_test, bst.predict(self.X_test), average="binary"):.4f}')
+            print("Model saved!")
---
+++ main.py
@@ -0,0 +1,24 @@
+import optuna
+import warnings
+import pandas as pd
+from Module.Esemble import *
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+
+    X_train = pd.read_csv('Database/X_train.csv', index_col=0)
+    y_train = pd.read_csv('Database/y_train', index_col=0)
+    X_val = pd.read_csv('Database/X_val.csv', index_col=0)
+    y_val = pd.read_csv('Database/y_val.csv', index_col=0)
+
+
+    for i in range(1, 4):
+        # {DT=0, lightGBM=1, XGBoost=2, CatBoost=3}
+        E = Esemble(i, X_train, X_val, y_train, y_val, 1000, 'original')
+
+        study = optuna.create_study(direction='maximize')
+        study.optimize(E.objective, n_trials=100)
+
+        print('Number of finished trials:', len(study.trials))
+        print('Best trial:', study.best_trial.params)
+        E.save_best_model(study.best_trial.params)
